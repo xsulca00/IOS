45e88502f9a486d838a4900cdcd26ea2a20dfc62
Adam Šulc <xsulca00@stud.fit.vutbr.cz>
2017-04-28 20:09:24 +0200

    Process generating & waiting posses correct behaviour now

--- proj2.c
+++ proj2.c
@@ -45,12 +45,15 @@ struct Number {
 
 /* prototypy funkci */
 void print_event(enum events event);	// tiskne udalosti do souboru proj2.out dle zadani
-void destroy_semaphores(void);		// zrusi alokovane semafory
 void get_params(char **argv);		// kontrola vstupnich parametru z terminalu
 void* alloc_shared_mem(size_t);
 sem_t* create_sem(unsigned);        // create semaphore with initial value
+void destroy_sem(sem_t*);
 struct Number get_num_or_err(char* str);
+void dealloc_shared_mem(void*, size_t);
 void free_all_resources(void);
+void create_adult_generator(void);
+void create_child_generator(void);
 
 // print error with prefix e.g. "Adults count: ...", NULL serves as end of strings
 void error_and_die(int, ...);
@@ -64,18 +67,11 @@ int* adults_count;      // poc
 int* childern_count;    // pocet deti pritomnych v centru
 
 /* sdilene semafory mezi procesy */
-sem_t *board;		// semafor ridici nastupovani	
-sem_t *unboard;		// semafor ridici vystupovani	
 sem_t *mutex;		// mutex do kriticke oblasti pri zapisu do souboru
 sem_t *mutex1;		// mutex do kriticke oblasti pri zapisu do souboru
 sem_t *mutex2;		// mutex do kriticke oblasti v procesu pasazera
-sem_t *load;		// semafor davajici povel k nastupovani do voziku
-sem_t *unload;		// semafor davajici povel k vystupovani z voziku
 sem_t *wait_adults_till_finish;     // adult procesy cekaji na ukonceni
 sem_t *wait_childern_till_finish;   // child procesy cekaji na ukonceni
-
-/* lokalni identifikator procesu <1, MAX_PROCESS> */
-int local_pasid;
 
 /* ukazatel na otevreny soubor */
 FILE *file;      
@@ -101,33 +97,17 @@ int main(int argc, char **argv)
     get_params(argv);
 
     errno = 0;	
-    file = fopen("proj2.out", "w");
-
-    if (file == NULL)
+    if ((file = fopen("proj2.out", "w"))  == NULL)
     {
         error_and_die(SYS_CALL_FAIL, "fopen:", NULL);
     }
-
-    pid_t adult_gen = -1;
-    pid_t child_gen = -1;
-
-    pid_t adult = -1;
-    pid_t child = -1;
-
-    /*
-    pid_t car = -1;		// id procesu voziku v ramci systemu
-    pid_t passenger = -1;	// id procesu pasazera v ramci systemu
-    pid_t help_process_pid = -1;	// id pomocneho procesu pro generovani pasazeru v ramci systemu
-    */
-    pid_t *adults_pids;		// pole pro ukladani id procesu adult
-    pid_t *childern_pids;	// pole pro ukladani id procesu child
 
     /* alokace sdilene pameti mezi procesy pro semafory */
     mutex = create_sem(1);
     mutex1 = create_sem(1);
     mutex2 = create_sem(1);
-    wait_childern_till_finish = create_sem(1);
-    wait_adults_till_finish = create_sem(1);
+    wait_childern_till_finish = create_sem(opts.childern_count);
+    wait_adults_till_finish = create_sem(opts.adults_count);
 
     /* alokace sdilene pameti pro sdilene promenne */
     event_id = alloc_shared_mem(sizeof(int));
@@ -135,253 +115,42 @@ int main(int argc, char **argv)
     child_id = alloc_shared_mem(sizeof(int));
     adults_count = alloc_shared_mem(sizeof(int));
     childern_count = alloc_shared_mem(sizeof(int));
-    adults_pids = alloc_shared_mem(sizeof(pid_t)*opts.adults_count);
-    childern_pids = alloc_shared_mem(sizeof(pid_t)*opts.childern_count);
+
+    // index od adult and child begins from one
+    *adult_id = 1;
+    *child_id = 1;
   
-    // adult processes generator 
-    errno = 0;
-    if ((adult_gen = fork()) < 0) 
+    create_adult_generator();
+    create_child_generator();
+
+    sem_post(wait_adults_till_finish);
+    sem_post(wait_childern_till_finish);
+
+    // wait for TWO generating processes to end
+    for (int i = 0; i != 2; i++)
     {
-        error_and_die(SYS_CALL_FAIL, "fork adult_gen:", NULL);
-    }
-    else if (adult_gen == 0)
-    {
-        for (int i = 1; i <= opts.adults_count; i++)
-        {
-            if (opts.adult_works > 0)
-                usleep(rand() % opts.adult_works + 1);   
-
-            // adult process
-            errno = 0;
-            if ((adult = fork()) < 0) 
-            {
-                error_and_die(SYS_CALL_FAIL, "fork adult:", NULL);
-            } 
-            else if (adult == 0) 
-            {
-                // adult process started
-                sem_wait(mutex);
-                print_event(ADULT_STARTED);
-                sem_post(mutex);
-
-                // get pid
-                sem_wait(mutex1);
-                adults_pids[(*adult_id)++] = getpid();
-                sem_post(mutex1);
-                exit(0);
-                break;
-            }
-        }
-
-        // adult procesy cekaji na ukonceni
-        if (adult != 0) 
-        {
-            for (int i = 0; i != opts.adults_count; i++)
-                waitpid(adults_pids[i], NULL, 0);
-            // aktivuj ukonceni dalsiho adult procesu
-            sem_post(wait_adults_till_finish); 
-            exit(NO_ERROR);
-        }
-    }
-  
-    // child processes generator
-    errno = 0;
-    if ((child_gen = fork()) < 0) 
-    {
-        error_and_die(SYS_CALL_FAIL, "fork child_gen:", NULL);
-    }
-    else if (child_gen == 0)
-    {
-        for (int i = 1; i <= opts.childern_count; i++)
-        {
-            if (opts.child_works > 0)
-                usleep(rand() % opts.child_works + 1); 
-
-            // child process
-            errno = 0;
-            if ((child = fork()) < 0) 
-            {
-                error_and_die(SYS_CALL_FAIL, "fork child:", NULL);
-            } 
-            else if (child == 0) 
-            {
-                // adult process started
-                sem_wait(mutex);
-                print_event(CHILD_STARTED);
-                sem_post(mutex);
-
-                // get pid
-                sem_wait(mutex2);
-                adults_pids[(*child_id)++] = getpid();
-                sem_post(mutex2);
-                exit(0);
-                break;
-            }
-        }
-        // pokud se vygenerovali vsici pasazeri tak cekat dokonce, nez se ukonci ostatni procesy
-        if (child != 0) 
-        {
-            for (int i = 0; i != opts.adults_count; i++)
-                waitpid(childern_pids[i], NULL, 0);
-            // aktivuj ukonceni dalsiho procesu
-            sem_post(wait_childern_till_finish); 
-            exit(NO_ERROR);
-        }
+        wait(NULL);
+        printf("GEN %i finished!\n", i);
     }
 
-  if (passenger == 0) {
-    sem_wait(mutex2);			
-    local_pasid = ++*pas_id;		// lokalni cislo procesu = unikatni cislo pro jeho identifikaci <1, P>
-    psn_pids[local_pasid-1] = getpid();	// ulozime id procesu v ramci systemu, aby mohl pomocny proces cekat na pasazery nez se ukonci
-    
-    /* tisk udalosti, vstup do kriticke oblasti */
-    sem_wait(mutex);
-    print_event(PAS_STARTED);    
-    sem_post(mutex);
-    /* vystup z kriticke oblasti po zapisu do souboru */
-    
-    
-    sem_post(mutex2); 
-    
-    // pasazer ceka na povel k nastupovani
-    sem_wait(load);
-  
-    // vstup do kriticke oblasti (promenna *actual_capacity)
-    sem_wait(mutex2);
-    // pokud jsou splneny podminky nastupovani, pasazeri zacinaji nastupovat
-    if (*actual_capacity <= carriage_capacity) {   
-      print_event(PAS_BOARD);
-      (*actual_capacity)++;   
-      // pokud je pasazer posledni, vypis udalost
-      if (*actual_capacity == carriage_capacity) {
-	sem_wait(mutex);
-	print_event(PAS_BOARD_LAST);
-	sem_post(mutex);
-	*actual_capacity = 0;
-	// oznam voziku, ze nastupovani je u konce
-	sem_post(board);	
-      } else {
-	sem_wait(mutex);
-	print_event(PAS_BOARD_ORDER);
-	sem_post(mutex);
-      }
-      sem_post(mutex2);          
-      // pasazer ceka, dokud vozik nevyda povel k vystupovani
-      sem_wait(unload);
-    }
-    
-    // vozik vyvolal operaci unload, pasazeri mohou vystupovat
-    sem_wait(mutex2);
-    if (*actual_capacity <= carriage_capacity) {   
-      sem_wait(mutex);
-      print_event(PAS_UNBOARD);
-      sem_post(mutex);
-      (*actual_capacity)++;  
-      // pokud je posledni pasazer, vypis patricne informace
-      if (*actual_capacity == carriage_capacity) {
-	  sem_wait(mutex);
-	  print_event(PAS_UNBOARD_LAST);
-	  sem_post(mutex);
-	  *actual_capacity = 0;
-	  // oznam voziku, ze vsichni pasazeri vystoupili
-	  sem_post(unboard);
-      } else {
-	  sem_wait(mutex);
-	  print_event(PAS_UNBOARD_ORDER);
-	  sem_post(mutex);
-      }
-    }
-    sem_post(mutex2);
-    // pasazer ceka na spolecne ukonceni procesu
-    sem_wait(wait_till_finish);
-    // aktivuj ukonceni dalsiho procesu
-    sem_post(wait_till_finish);
-    sem_wait(mutex2);
-    print_event(PAS_FINISHED);
-    sem_post(mutex2);
+    destroy_sem(mutex);
+    destroy_sem(mutex1);
+    destroy_sem(mutex2);
+    destroy_sem(wait_adults_till_finish);
+    destroy_sem(wait_childern_till_finish);
+
+    dealloc_shared_mem(event_id, sizeof(*event_id));
+    dealloc_shared_mem(adult_id, sizeof(*adult_id));
+    dealloc_shared_mem(child_id, sizeof(*child_id));
+
+    fclose(file);
+
     exit(NO_ERROR);
-  }
-  
-  // pokud se nepodarilo vytvorit proces vykonej patricne kroky
-  errno = 0;
-  if ((car = fork()) < 0) {
-     error_and_die(SYS_CALL_FAIL, "fork car:", NULL);
-  } else if (car == 0) {
-    
-    (*car_id)++; // unikatni cislo voziku
-    
-    sem_wait(mutex);
-    print_event(CAR_STARTED);	// vozik zacal fungovat
-    sem_post(mutex);
-    
-    // vozik pracuje v cyklu
-    for (int i = 1; i <= passenger_count/carriage_capacity; i++) {   
-      sem_wait(mutex);
-      print_event(CAR_LOAD);
-      sem_post(mutex);
-      
-      // proces vyvola operaci load (zahajeni nastupovani), semaforem projde tolik procesu, kolik je kapacita voziku
-      for (int i = 1; i <= carriage_capacity; i++)
-	sem_post(load);
-      
-      // proces ceka, dokud nenastoupi vsichni pasazeri
-      sem_wait(board); 
-      
-      // jakmile je vozik plny, proces vyvola operaci run
-      sem_wait(mutex);
-      print_event(CAR_RUN);     
-      sem_post(mutex);
-      if (duration_of_ride > 0)
-	usleep(rand() % duration_of_ride + 1); // uspi se na nahodnou dobu z intervalu <0, RT>
-      
-      // proces vyvola operaci unload
-      sem_wait(mutex);
-      print_event(CAR_UNLOAD);
-      sem_post(mutex);
-      
-      // po probuzeni vyvola proces operaci unload (zahajeni vystupovani), semaforem projde tolik procesu, kolik je kapacita voziku 
-      for (int i = 1; i <= carriage_capacity; i++)
-	sem_post(unload);
-
-      // proces nemuze zahajit operaci load, dokud nevystoupi vsichni pasazeri 
-      sem_wait(unboard);
-    }
-
-    // vozik dokoncil praci, aktivuj ukonceni vsech procesu
-    sem_post(wait_till_finish);
-    // ceka na ukonceni pomocneho procesu pro generovani pasazeru
-    waitpid(help_process_pid, NULL, 0);
-    sem_wait(mutex);
-    print_event(CAR_FINISHED);
-    sem_post(mutex);
-    exit(NO_ERROR);
-  }    
-  
-  // hlavni proces ceka na ukonceni vsech procesu pak se ukonci sam
-  // hlavni proces ceka na ukonceni pomocneho procesu
-  waitpid(help_process_pid, NULL, 0);
-  // hlavni proces ceka na ukonceni voziku
-  waitpid(car, NULL, 0); 
- 
-
-  // vycisti alokovane semafory
-  destroy_semaphores();
-  //uvolni sdilenou pamet
-  munmap(board, sizeof(sem_t));
-  munmap(mutex, sizeof(sem_t));
-  munmap(unboard, sizeof(sem_t));
-  munmap(mutex2, sizeof(sem_t));
-  munmap(load, sizeof(sem_t));
-  munmap(wait_till_finish, sizeof(sem_t));
-  munmap(unload, sizeof(sem_t));
-  // zavri soubor
-  fclose(file);
-  
-  exit(NO_ERROR);
 }
 
 void print_event(enum events event)
 {
+  sem_wait(mutex);
   (*event_id)++;
   setbuf(file, NULL);
   switch (event) {
@@ -402,25 +171,31 @@ void print_event(enum events event)
     case CHILD_FINISHED: fprintf(file, "%i: C %i: finished\n", *event_id, *child_id); break;
   }
   fflush(file);
+  sem_post(mutex);
 }
 
-void destroy_semaphores(void)
+void destroy_sem(sem_t* sem)
 {
-  errno = 0;
-  
-  if (
-    sem_destroy(board) == -1 ||
-    sem_destroy(mutex) == -1 ||
-    sem_destroy(mutex1) == -1 ||
-    sem_destroy(mutex2) == -1 ||
-    sem_destroy(unboard) == -1 ||
-    sem_destroy(load) == -1 ||
-    sem_destroy(wait_adults_till_finish) == -1 ||
-    sem_destroy(wait_childern_till_finish) == -1 ||
-    sem_destroy(unload) == -1
-  ) {
+    errno = 0;
+    if (sem_destroy(sem) == -1)
+    {
         error_and_die(SYS_CALL_FAIL, __func__, "sem_destroy: ", NULL);
-  }
+    }
+
+    errno = 0;
+    if (munmap(sem, sizeof(sem_t)) == -1)
+    {
+        error_and_die(SYS_CALL_FAIL, __func__, "munmap: ", NULL);
+    }
+}
+
+void dealloc_shared_mem(void* mem, size_t size)
+{
+    errno = 0;
+    if (munmap(mem, size) == -1)
+    {
+        error_and_die(SYS_CALL_FAIL, __func__, "munmap: ", NULL);
+    }
 }
 
 void error_and_die(int severity, ...)
@@ -477,6 +252,109 @@ void free_all_resources(void)
 {
 }
 
+// generate child process
+void create_adult_generator()
+{
+    pid_t adult_gen = -1;
+    pid_t adult = -1;
+
+    errno = 0;
+    if ((adult_gen = fork()) < 0) 
+    {
+        error_and_die(SYS_CALL_FAIL, "fork adult_gen:", NULL);
+    }
+    else if (adult_gen == 0)
+    {
+        // generate adult processes
+        for (int i = 0; i != opts.adults_count; i++)
+        {
+            if (opts.adult_works > 0)
+                usleep(rand() % (opts.adult_works + 1));   
+
+            // adult process
+            errno = 0;
+            if ((adult = fork()) < 0) 
+            {
+                error_and_die(SYS_CALL_FAIL, "fork adult:", NULL);
+            } 
+            else if (adult == 0) 
+            {
+                // adult process started
+                print_event(ADULT_STARTED);
+
+                // get pid
+                sem_wait(mutex1);
+                (*adult_id)++;
+                sem_post(mutex1);
+                sem_wait(wait_adults_till_finish);
+                exit(0);
+            }
+        }
+
+        // waiting for adult processes to end 
+        if (adult != 0) 
+        {
+            for (int i = 0; i != opts.adults_count; i++)
+            {
+                wait(NULL);
+                printf("ADULT: %i finished!\n", i);
+            }
+            exit(NO_ERROR);
+        }
+    }
+}
+
+// child processes generator
+void create_child_generator()
+{
+    pid_t child_gen = -1;
+    pid_t child = -1;
+
+    errno = 0;
+    if ((child_gen = fork()) < 0) 
+    {
+        error_and_die(SYS_CALL_FAIL, "fork child_gen:", NULL);
+    }
+    else if (child_gen == 0)
+    {
+        // generate childern processes
+        for (int i = 0; i != opts.childern_count; i++)
+        {
+            if (opts.child_works > 0)
+                usleep(rand() % opts.child_works + 1); 
+
+            // child process
+            errno = 0;
+            if ((child = fork()) < 0) 
+            {
+                error_and_die(SYS_CALL_FAIL, "fork child:", NULL);
+            } 
+            else if (child == 0) 
+            {
+                // child process started
+                print_event(CHILD_STARTED);
+
+                // get pid
+                sem_wait(mutex2);
+                (*child_id)++;
+                sem_post(mutex2);
+                sem_wait(wait_childern_till_finish);
+                exit(0);
+            }
+        }
+
+        // waiting for childern processes to end
+        if (child != 0) 
+        {
+            for (int i = 0; i != opts.childern_count; i++)
+            {
+                wait(NULL);
+                printf("CHILD: %i finished!\n", i);
+            }
+            exit(NO_ERROR);
+        }
+    }
+}
 
 // string to number and possibly error
 struct Number get_num_or_err(char* str)
